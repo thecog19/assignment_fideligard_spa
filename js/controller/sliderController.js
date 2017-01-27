114a3b08f5c4436c0b6b9e37248ba07403c3a730
Jonathan Popenuck <john.popenuck@gmail.com>
2017-01-27 11:57:18 -0800

    worked on dates display

--- js/controller/sliderController.js
+++ js/controller/sliderController.js
@@ -1,8 +1,16 @@
-store.controller('CheckoutCtrl', ['$scope',
+Fideligard.controller('SliderController', ['$scope',
   function($scope){
-    $scope.dateSelector = true
+    $scope.dateSelector = true;
+    $scope.slide = 0  ;
+    $scope.endDate = new Date() - 0;
+    $scope.startDate = new Date() - 15897600000;
+    $scope.currentDate = new Date() - 0;
 
     $scope.toggleSelector = function(){
-      $scope.dateSelector = !$scope.dateSelector
+      $scope.dateSelector = !$scope.dateSelector;
+    }
+
+    $scope.changeCurrentDate = function() {
+      $scope.currentDate = new Date() +($scope.slide)
     }
   }]);
