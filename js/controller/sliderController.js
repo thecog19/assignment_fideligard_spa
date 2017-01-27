b6b0400feaa40e2f832aa0459f56a95f412853ed
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-27 16:54:42 -0500

    wooo fun times

--- js/controller/sliderController.js
+++ js/controller/sliderController.js
@@ -1,10 +1,11 @@
-Fideligard.controller('SliderController', ['$scope',
-  function($scope){
+Fideligard.controller('SliderController', ['$scope', "stocksService",
+  function($scope, stocksService){
     $scope.dateSelector = true;
     $scope.slide = 0  ;
     $scope.endDate = new Date() - 0;
     $scope.startDate = new Date() - 15897600000;
     $scope.currentDate = new Date() - 0;
+    stocksService.obtainStocks().then(function(response){console.log(response.data.query.results)})
 
     $scope.toggleSelector = function(){
       $scope.dateSelector = !$scope.dateSelector;
---
+++ js/services/stocksService.js
@@ -0,0 +1,43 @@
+Fideligard.factory('stocksService', [ "$http",
+  function($http){
+    var _start = "2014-09-11"
+    var _end = "2015-07-11"
+
+  var obtainStocks = function(){
+      return $http({
+        method: "GET",
+        url: urlBuilder(["AAPL", "YAOO", "GOOG", "MSFT"]),
+        // data: {
+        //   q: 'select * from yahoo.finance.historicaldata where  symbol = "AAPL" and startDate = "2011-09-11" and endDate = "2014-02-11"',
+        //   format: "json",
+        //   diagnostics: "true",
+        //   "env=store": "//datatables.org/alltableswithkeys",
+        //   callback: ""
+        // },
+        success: function(response){return response},
+        failire: function(response){console.log(response)}
+      })
+    }
+
+  var urlBuilder = function(companyArray) {
+    var companyString = "'" + companyArray.join("','") + "'";
+    var startDate = _start.replace(/-/g, '/');
+    var endDate = _end.replace(/-/g, '/');
+
+    return 'http://query.yahooapis.com/v1/public/yql?q=' +
+              'select * from yahoo.finance.historicaldata ' +
+              'where symbol in (' + companyString + ') ' +
+              'and startDate = "' + startDate + '" ' +
+              'and endDate = "' + endDate + '" ' +
+              '&format=json '+
+              '&diagnostics=true ' +
+              '&env=store://datatables.org/alltableswithkeys ' +
+              '&callback=';
+  };
+
+
+  return {
+    obtainStocks: obtainStocks
+  }
+
+}])
