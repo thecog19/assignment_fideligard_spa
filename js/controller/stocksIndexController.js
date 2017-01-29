dd6a944a2d5f1b75e548b1d5fba8a706398faba6
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-28 23:20:17 -0500

    listeners! yay!

--- js/controller/stocksIndexController.js
+++ js/controller/stocksIndexController.js
@@ -1,6 +1,10 @@
 Fideligard.controller("stocksIndexController", ["$scope", "stocksService", "dateService", function($scope, stocksService, dateService) {
   $scope.stockData = [];
-  $scope.currentDate = dateService.currentDate;
+  $scope.currentDate = dateService.getCurrentDate();
+
+  $scope.$on('dateChange', function(event) {
+    $scope.currentDate = dateService.getCurrentDate();
+  })
 
   stocksService.obtainStocks().then(function(response){
     $scope.stockData = response.data.query.results.quote
--- js/services/dateService.js
+++ js/services/dateService.js
@@ -1,11 +1,12 @@
-Fideligard.factory('dateService', [ "$http",
-  function($http){
+Fideligard.factory('dateService', [ "$rootScope",
+  function($rootScope){
     var _endDate = new Date() - 0;
     var _startDate = new Date() - 15897600000;
     var currentDate = new Date() - 0;
 
     var changeCurrentDate = function(slideValue) {
-      currentDate = new Date() - (slideValue * -1)
+      $rootScope.$broadcast('dateChange', currentDate);
+      currentDate = _startDate + slideValue
     }
 
     var getDates = function() {
@@ -17,6 +18,7 @@ Fideligard.factory('dateService', [ "$http",
     }
 
     var getCurrentDate = function() {
+      console.log("rab")
       return currentDate
     }
 
